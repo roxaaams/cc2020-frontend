4f62e8db228a2adea206d430a9d054b8dd704f20
Lukas Jäger <lukas@simpladd.com>
2020-12-15 20:06:09 +0100

    updated hooks

--- src/hooks/hook.master.ts
+++ src/hooks/hook.master.ts
@@ -1,40 +1,6 @@
 import { useEffect, useState, useMemo } from 'react';
 import * as AWS from 'aws-sdk';
-import { Consumer } from 'sqs-consumer';
-import { EntryResult } from '../types/type.result';
-import { MessageType } from '../types/type.message';
 import { MasterTrigger } from '../types/type.master';
-
-export const useResultListener = () => {
-  const [results, setResults] = useState<EntryResult[]>([]);
-
-  useEffect(() => {
-    const sqs = new AWS.SQS({
-      accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
-      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
-      sessionToken: process.env.REACT_APP_SESSION_TOKEN,
-      region: process.env.REACT_APP_REGION,
-    });
-
-    const consumer = Consumer.create({
-      queueUrl: process.env.REACT_APP_SQS_URL,
-      sqs: sqs,
-      handleMessage: async (message) => {
-        const msg = JSON.parse(JSON.parse(JSON.stringify(message)).Body);
-
-        const messageInfo = JSON.parse(msg.Message);
-
-        if (messageInfo.type === MessageType.EntryResult) {
-          console.log('received new result: ', messageInfo);
-        }
-      },
-    });
-
-    consumer.start();
-  }, []);
-
-  return results;
-};
 
 export const useMatrixCalculationTrigger = () => {
   const sns = useMemo(() => {
@@ -47,12 +13,18 @@ export const useMatrixCalculationTrigger = () => {
   }, []);
 
   const onTrigger = (input: MasterTrigger) => () => {
+    console.log(
+      `{"multiplicand": {"id": "${input.multiplicand.id}"}, "multiplier": {"id": "${input.multiplier.id}"}}`
+    );
+
     sns
       .publish({
         TopicArn: process.env.REACT_APP_MASTER_SNS_TOPIC_ARN,
-        Message: `{"multiplicand": "id": ${input.multiplicand.id}, "multiplier": ${input.multiplier.id}}`,
+        Message: `{"multiplicand": {"id": "${input.multiplicand.id}"}, "multiplier": {"id": "${input.multiplier.id}"}}`,
       })
       .promise();
+
+    console.log('sent');
   };
 
   return onTrigger;
--- src/hooks/hook.matrix.ts
+++ src/hooks/hook.matrix.ts
@@ -2,36 +2,105 @@ import { useEffect, useState, ChangeEvent, useMemo } from 'react';
 import { Matrix, MatrixInput } from '../types/type.matrix';
 import * as AWS from 'aws-sdk';
 import { Consumer } from 'sqs-consumer';
+import { MessageType } from '../types/type.message';
+import { EntryResult, ResultMatrix } from '../types/type.result';
 
 export const useMatrixListener = () => {
-  const [matrices, setMatrices] = useState<Matrix[]>([]);
+  const [matrices, setMatrices] = useState<Matrix[]>(() => []);
+  const [relevantCalculations, setRelevantCalculations] = useState<
+    EntryResult[]
+  >(() => []);
+  const [resultMatrices, setResultMatrices] = useState<ResultMatrix[]>(
+    () => []
+  );
+  const [calculationCache, setCalculationCache] = useState<
+    Map<string, Map<string, number>>
+  >(() => new Map());
+
+  const [multiplicationId, setMultiplicationId] = useState<string>(() => '');
+
+  const setMultiplicationListener = (id: string) => {
+    setMultiplicationId(id);
+  };
+
+  const sqs = useMemo(
+    () =>
+      new AWS.SQS({
+        accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+        secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+        sessionToken: process.env.REACT_APP_SESSION_TOKEN,
+        region: process.env.REACT_APP_REGION,
+      }),
+    []
+  );
+
+  const cachedMultiplicationId = useMemo(() => multiplicationId, [
+    multiplicationId,
+  ]);
 
   useEffect(() => {
-    const sqs = new AWS.SQS({
-      accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
-      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
-      sessionToken: process.env.REACT_APP_SESSION_TOKEN,
-      region: process.env.REACT_APP_REGION,
-    });
+    let cachedM: Matrix[] = [];
+    let cachedRelevantCalculations: EntryResult[] = [];
+    let chachedCachedResults: Map<string, Map<string, number>> = new Map();
 
     const consumer = Consumer.create({
       queueUrl: process.env.REACT_APP_SQS_URL,
       sqs: sqs,
       handleMessage: async (message) => {
-        const msg = JSON.parse(JSON.parse(JSON.stringify(message)).Body);
+        const msg = JSON.parse(`${message.Body}`).Message;
 
-        const messageInfo = JSON.parse(msg.Message);
+        const msgString = `${msg}`.replaceAll("'", '"');
 
-        if (messageInfo.type === 'new matrix generated') {
-          const newMatrices = matrices;
+        let messageInfo = JSON.parse(msgString);
 
-          const matrix = messageInfo.matrix;
-          newMatrices.push({
-            id: matrix.id,
-            rows: matrix.rows,
-            columns: matrix.columns,
-          });
-          setMatrices(newMatrices);
+        if (
+          messageInfo.result &&
+          messageInfo.result.type === MessageType.EntryResult
+        )
+          messageInfo = messageInfo.result;
+
+        if (messageInfo.type) {
+          console.log('received new result: ', messageInfo.type);
+
+          switch (messageInfo.type) {
+            case MessageType.ResultMatrix:
+              let newResultM = [...(resultMatrices || [])];
+              newResultM.push({
+                result_matrix_id: messageInfo.result_matrix_id,
+                multiplicand_matrix_id: messageInfo.mmultiplicand_matrix_id,
+                multiplier_matrix_id: messageInfo.multiplier_matrix_id,
+              });
+              setResultMatrices(newResultM);
+              break;
+            case MessageType.EntryResult:
+              if (messageInfo.id === cachedMultiplicationId) {
+                console.log('hit!');
+                cachedRelevantCalculations.push({
+                  column_index: messageInfo.column_index,
+                  row_index: messageInfo.row_index,
+                  value: messageInfo.value,
+                });
+                setRelevantCalculations([
+                  ...(cachedRelevantCalculations || []),
+                ]);
+              } else {
+                let arr = chachedCachedResults.get(messageInfo.id) || [];
+                let map = chachedCachedResults.get(messageInfo.id) || new Map();
+                map.set(
+                  `${messageInfo.row_index}-${messageInfo.column_index}`,
+                  messageInfo.value
+                );
+                chachedCachedResults.set(messageInfo.id, map);
+                setCalculationCache(new Map(chachedCachedResults));
+              }
+              break;
+            case MessageType.NewMatrix:
+              const matrix = messageInfo.matirx;
+              console.log('matrix:', matrix);
+              cachedM.push(matrix);
+              setMatrices([...(cachedM || [])]);
+              break;
+          }
         }
       },
     });
@@ -39,7 +108,11 @@ export const useMatrixListener = () => {
     consumer.start();
   }, []);
 
-  return matrices;
+  return {
+    availableMatrices: matrices,
+    calculationCache,
+    resultMatrices,
+  };
 };
 
 export const useMatrixInput = () => {
